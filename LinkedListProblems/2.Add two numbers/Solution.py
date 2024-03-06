d2de0733752b9606d2eb4aadb128220d8f22d657
SRI PHANI DEEPTHIMAI KOPPULA <161422554+SriPhaniDeepthimai@users.noreply.github.com>
2024-03-06 21:11:29 +0530

    Create Solution.py

---
+++ LinkedListProblems/2.Add two numbers/Solution.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode 
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        result=ListNode(0)
+        ptr=result
+        carry=0
+        while(l1 !=None or l2!=None):
+            sum=carry
+            if (l1!=None):
+                sum=sum+l1.val
+                l1=l1.next
+            if(l2!=None):
+                sum=sum+l2.val
+                l2=l2.next
+            carry=sum/10
+            sum=sum%10
+            ptr.next=ListNode(sum)
+            ptr=ptr.next
+        if carry==1:
+            ptr.next=ListNode(carry)
+
+        return result.next
+
+        
